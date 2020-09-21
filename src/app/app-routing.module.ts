231dbb75b32eec9c89f41566f63df9c18b63526e
lizhen <lizhen@zhumengyuan.com>
2020-09-21 08:56:22 +0800

    baseline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DrawerComponent} from "./drawer/drawer.component";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'drawer', component: DrawerComponent },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
